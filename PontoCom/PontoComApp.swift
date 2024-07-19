fe81d4983c8b4ef8b6f5459fd4551d93121f2c55
Joel Lacerda <joellacerdaol@gmail.com>
2024-07-19 18:46:36 -0300

    Initial Commit

---
+++ PontoCom/PontoComApp.swift
@@ -0,0 +1,17 @@
+//
+//  PontoComApp.swift
+//  PontoCom
+//
+//  Created by Joel Lacerda on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PontoComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
