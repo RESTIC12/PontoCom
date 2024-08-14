26c7754d740f4b899265cc33a3c6bba75b67f382
Levi Soares <levi-castro@outlook.com>
2024-08-14 20:39:47 -0300

    adicionando picker da imagem

--- PontoCom/View/PerfilView.swift
+++ PontoCom/View/PerfilView.swift
@@ -6,13 +6,108 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+import FirebaseStorage
 
-struct PerfilView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+
+    
+
+    struct PerfilView: View {
+        
+        @State var shouldShowImagePicker = false
+        @State var image: UIImage?
+        
+        var body: some View {
+            
+            VStack{
+                
+                //                NavigationStack {
+                //
+                //
+                //                }
+                VStack(spacing: 16){
+//                    Image("2D")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fill)
+//                        .frame(width: 200, height: 200)
+//                        .clipShape(Circle())
+//                        .clipped()
+//                        .padding(.top, 44)
+//                    Label("Levi Soares", systemImage: "person").font(.system(size: 20)).bold().foregroundColor(.blue)
+//                        .padding(.top, 12)
+//                        .cornerRadius(10)
+                    Text("Foto de perfil")
+                    
+                    Button {
+                        shouldShowImagePicker.toggle()
+                    } label: {
+                        VStack{
+                            if let image = self.image {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 128, height: 128)
+                                    .cornerRadius(64)
+                            }else{
+                                
+                            }
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                                .foregroundColor(Color(.label))
+                            
+                    }
+                        .overlay(RoundedRectangle(cornerRadius: 64).stroke(Color.black, lineWidth: 3))
+                    }
+                    
+                    Spacer()
+                    
+                }
+                
+                Button(action: {
+                    
+                }){
+                    Label("Editar informacoes", systemImage: "pencil.and.list.clipboard")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                        .opacity(0.8)
+                    
+                    
+                }
+                
+                Button(action:{
+                    
+                }){
+                    Label("Justificar faltas", systemImage: "doc.questionmark")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                        .opacity(0.8)
+                    
+                }
+                
+                
+                Button(action:{
+                    
+                }){
+                    Label("Historico", systemImage: "calendar")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(.blue)
+                        .opacity(0.7)
+                        .cornerRadius(10)
+                        .padding()
+                }
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil){
+                ImagePickerView(image: $image)
+            }
+        }
+        
     }
-}
 
-#Preview {
-    PerfilView()
-}
---
+++ PontoCom/ViewModel/PerfilViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PerfilViewModel.swift
+//  PontoCom
+//
+//  Created by levi soares on 13/08/24.
+//
+
+import Foundation
